3bb4e217d64ee53f725a90ac2329e78cda048757
Tom Dennis <tdennis913@gmail.com>
2018-10-31 18:53:36 -0400

    added screenshots

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -232,7 +232,7 @@ for (let i = 0; i < pokemonImages.length; i++) {
 */
 
 // =======================================================
-String.prototype.replaceAt=function(index, replacement) {
+String.prototype.replaceAt = function(index, replacement) {
     return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
 // =======================================================
